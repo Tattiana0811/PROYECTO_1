9d2a083d1517d26068bc24b4e01f189200bab9c8
Luvi Tattiana Granados Dominguez <luvit.granadosd@uqvirtual.edu.co>
2024-08-14 17:07:49 -0500

    agrego nuevo archivo#

---
+++ main.py
@@ -0,0 +1,8 @@
+input ("Hola,¿como te llamas? ") 
+print ("Adivinare tu edad, piensa en dos números que sumados den tu edad.")
+n1 = float(input ("Dame el primer número que pensaste: "))
+n2 = float(input ("Ahora dame el otro número pensado: "))
+suma = n1+n2
+print ("Tu edad es ",suma)
+
+
